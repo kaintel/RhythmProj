2084bd1746f4cb59c9d96c2333db944b61c68896
kaintel <frankie331199@gmail.com>
2025-01-24 12:23:54 -0500

    platform

--- minotaur.js
+++ minotaur.js
@@ -1,7 +1,6 @@
 class Minotaur{
     constructor(x,y,z){
-this.r=0
-this.dr=0.1
+
         this.obj = document.createElement("a-gltf-model");
         this.obj.setAttribute("src","#mino");
         this.obj.setAttribute("position",{x:x,y:y,z:z});
@@ -11,8 +10,4 @@ this.dr=0.1
     this.obj.setAttribute("animation-mixer",{timeScale: 4});
       }
 
-      rotate(){
-        this.r += this.dr;
-          this.obj.setAttribute("rotation", this.r);
-          }
 }
