cbb8b27aaef3779605daa28126ee1954ca6d01f5
kaintel <frankie331199@gmail.com>
2025-02-05 20:57:18 -0500

    healthbar added

--- simplify.js
+++ simplify.js
@@ -22,13 +22,20 @@ function display(){
     }
     this.flag = false;
     miss = 0;
+    for (let yourhealth of yourhealths) {
+    yourhealth.returnhealth();
+    }
   }
 
   function play(){
+    for (let yourhealth of yourhealths) {
+      
+    
     for (let note1 of note1s) {
         note1.move();
         if (note1.y < -0.55) {
           note1.miss();
+          yourhealth.damage();
           document.querySelectorAll('#output')[1].setAttribute('value', `combo: ${combo}`);
           note1s.splice(note1s.indexOf(note1), 1);
         }
@@ -37,6 +44,7 @@ function display(){
         note2.move();
         if (note2.y < -0.55) {
           note2.miss();
+          yourhealth.damage();
           document.querySelectorAll('#output')[1].setAttribute('value', `combo: ${combo}`);
           note2s.splice(note2s.indexOf(note2), 1);
         }
@@ -45,6 +53,7 @@ function display(){
         note3.move();
         if (note3.y < -0.55) {
           note3.miss();
+          yourhealth.damage();
           document.querySelectorAll('#output')[1].setAttribute('value', `combo: ${combo}`);
           note3s.splice(note3s.indexOf(note3), 1);
         }
@@ -53,8 +62,10 @@ function display(){
         note4.move();
         if (note4.y < -0.55) {
           note4.miss();
+          yourhealth.damage();
           document.querySelectorAll('#output')[1].setAttribute('value', `combo: ${combo}`);
           note4s.splice(note4s.indexOf(note4), 1);
         }
       }
+    }
   }
